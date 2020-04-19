a44af111dcafe398e073814da8af8329121fcab7
Luis Masuelli <luismasuelli@hotmail.com>
2020-04-19 18:13:39 -0500

    Adding the protocols funnel factory.

--- funnel.go
+++ funnel.go
@@ -1,12 +1,19 @@
 package protocols
 
 import (
+	"errors"
 	"github.com/universe-10th/chasqui"
 	"github.com/universe-10th/chasqui/types"
 	"net"
 	"time"
 )
 
+var ErrNoProtocols = errors.New("no protocols specified")
+
+// Funnels several protocols simultaneously for the
+// managed server & attendants. It MAY handle several
+// servers at once, if the protocols are carefully
+// designed.
 type ProtocolsFunnel struct {
 	flattened               []Protocol
 	handlers                MessageHandlers
@@ -17,7 +24,7 @@ type ProtocolsFunnel struct {
 	onAcceptFailed          func(*chasqui.Server, error)
 	onMessageUnknown        MessageHandler
 	onMessagePanic          MessagePanicHandler
-	onThrottled             func(*chasqui.Server, *chasqui.Attendant, types.Message, time.Time, time.Duration)
+	onMessageThrottled      func(*chasqui.Server, *chasqui.Attendant, types.Message, time.Time, time.Duration)
 	onAttendantStoppedPanic func(*chasqui.Server, *chasqui.Attendant, chasqui.AttendantStopType, error, Protocol, interface{})
 	onStoppedPanic          func(*chasqui.Server, Protocol, interface{})
 }
@@ -108,8 +115,8 @@ func (funnel *ProtocolsFunnel) AttendantStarted(server *chasqui.Server, attendan
 // This event is strictly bypassed to a callback.
 func (funnel *ProtocolsFunnel) MessageThrottled(server *chasqui.Server, attendant *chasqui.Attendant, message types.Message,
 	instant time.Time, lapse time.Duration) {
-	if funnel.onThrottled != nil {
-		funnel.onThrottled(server, attendant, message, instant, lapse)
+	if funnel.onMessageThrottled != nil {
+		funnel.onMessageThrottled(server, attendant, message, instant, lapse)
 	}
 }
 
@@ -150,4 +157,100 @@ func (funnel *ProtocolsFunnel) AttendantStopped(server *chasqui.Server, attendan
 		count = index
 	}
 	delete(funnel.attendantLoadProgress, attendant)
+}
+
+// Option to set the "server started panic" callback to handle the panics
+// when a protocol tries to initialize for a server.
+func WithStartedPanic(callback func(*chasqui.Server, *net.TCPAddr, Protocol, interface{})) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onStartedPanic = callback
+	}
+}
+
+// Option to set the "accept failed" callback to handle when the server cannot
+// accept a connection.
+func WithAcceptFailed(callback func(*chasqui.Server, error)) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onAcceptFailed = callback
+	}
+}
+
+// Option to set the "attendee started panic" callback to handle the panics
+// when a protocol tries to initialize for an attendee.
+func WithAttendantStartedPanic(callback func(*chasqui.Server, *chasqui.Attendant, Protocol, interface{})) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onAttendantStartedPanic = callback
+	}
+}
+
+// Option to set the "unknown message" callback to handle when the protocols
+// cannot understand a message.
+func WithMessageUnknown(callback MessageHandler) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onMessageUnknown = callback
+	}
+}
+
+// Option to set the "message panic" callback to handle when the underlying
+// message handling incurs in a panic.
+func WithMessagePanic(callback MessagePanicHandler) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onMessagePanic = callback
+	}
+}
+
+// Option to set the "message throttled" callback to handle when the protocols
+// cannot understand a message.
+func WithMessageThrottled(callback func(*chasqui.Server, *chasqui.Attendant, types.Message, time.Time, time.Duration)) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onMessageThrottled = callback
+	}
+}
+
+// Option to set the "attendant stopped panic" callback to handle the panics
+// when a protocol tries to cleanup for an attendant.
+func WithAttendantStoppedPanic(callback func(*chasqui.Server, *chasqui.Attendant, chasqui.AttendantStopType, error, Protocol, interface{})) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onAttendantStoppedPanic = callback
+	}
+}
+
+// Option to set the "stopped panic" callback to handle the panics
+// when a protocol tries to cleanup for a server.
+func WithStoppedPanic(callback func(*chasqui.Server, Protocol, interface{})) func(target *ProtocolsFunnel) {
+	return func(target *ProtocolsFunnel) {
+		target.onStoppedPanic = callback
+	}
+}
+
+// Creates a new protocols funnel. It takes some of the protocols
+// involved, and also takes the options to configure the callbacks
+// for reporting.
+func NewProtocolsFunnel(protocols []Protocol, options ...func(target *ProtocolsFunnel)) (*ProtocolsFunnel, error) {
+	funnel := &ProtocolsFunnel{}
+	if len(protocols) == 0 {
+		return nil, ErrNoProtocols
+	}
+	flattened, err := flatten(protocols)
+	if err != nil {
+		return nil, err
+	} else {
+		funnel.flattened = flattened
+	}
+
+	handlers := make(MessageHandlers)
+	for _, protocol := range flattened {
+		if err := handlers.Merge(protocol.Handlers()); err != nil {
+			return nil, err
+		}
+	}
+	funnel.handlers = handlers
+
+	funnel.serverLoadProgress = make(map[*chasqui.Server]int)
+	funnel.attendantLoadProgress = make(map[*chasqui.Attendant]int)
+
+	for _, option := range options {
+		option(funnel)
+	}
+	return funnel, nil
 }
