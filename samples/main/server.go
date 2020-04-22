65c8015804134bad6f5aa80cff7b633cc5ac3bcc
Luis Masuelli <luismasuelli@hotmail.com>
2020-04-22 13:51:17 -0500

    Adding the server.

---
+++ samples/main/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/universe-10th/chasqui"
+	"github.com/universe-10th/chasqui-protocols"
+	"github.com/universe-10th/chasqui/marshalers/json"
+)
+
+var auth = &AuthProtocol{}
+var chat = &ChatProtocol{auth}
+var funnel, _ = protocols.NewProtocolsFunnel([]protocols.Protocol{chat, auth})
+
+func MakeServer() *chasqui.Server {
+	return chasqui.NewServer(
+		&json.JSONMessageMarshaler{}, 1024, 1, 0,
+	)
+}
+
+func funnelServer(server *chasqui.Server) {
+	chasqui.FunnelServerWith(server, funnel)
+}
